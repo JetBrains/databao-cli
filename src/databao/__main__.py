52bd3eb2f0841b305f4c46e0ab77f9d671d990b1
Sergey Stupin <stupin.sergey@gmail.com>
2026-01-29 13:21:23 +0100

    implement databao status (based on dce info so far)

--- src/databao/__main__.py
+++ src/databao/__main__.py
@@ -1,18 +1,39 @@
+from pathlib import Path
+
 import click
+from click import Context
 
 from databao.agents.hello import agents
-from databao.dce.hello import dce
+from databao.commands.status import echo_status
 
 
 @click.group()
-def cli():
+@click.option(
+    "-d",
+    "--project-dir",
+    type=click.Path(file_okay=False),
+    help="Location of your Databao Context Engine project",
+)
+@click.pass_context
+def cli(ctx: Context, project_dir: Path | None):
     """Databao Common CLI"""
-    pass
+    if project_dir is None:
+        project_path = Path.cwd()
+    else:
+        project_path = Path(project_dir.name).expanduser()
+
+    ctx.ensure_object(dict)
+    ctx.obj["project_dir"] = project_path
+
+
+@cli.command()
+@click.pass_context
+def status(ctx: Context) -> None:
+    """Display project status and system-wide information."""
+    echo_status(ctx.obj["project_dir"])
 
 
 cli.add_command(agents)
-cli.add_command(dce)
-
 
 if __name__ == "__main__":
     cli()
---
+++ tests/test_status.py
@@ -0,0 +1,15 @@
+from click.testing import CliRunner
+
+from databao.__main__ import cli
+
+
+def test_databao_status():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["status"])
+
+    assert result.exit_code == 0, result.output
+    assert (
+        "Databao context engine plugins: "
+        "['jetbrains/duckdb', 'jetbrains/parquet', 'jetbrains/unstructured_files', 'jetbrains/unstructured_files']"
+        "" in result.output
+    )
