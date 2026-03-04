0d8c5a3f715fc8d664e3cb1d4a5200a9caedda2b
SokolovYaroslav <SokolovYaroslav@users.noreply.github.com>
2026-03-04 15:13:25 +0000

    deploy: 1ef84658d11b869cc7c72f0d126830d8f096fca0

--- e2e-tests/src/databases/database_utils.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import yaml
-from utils.path_utils import get_src_folder
-
-
-def create_and_save_database_creds_file(project_path: Path, name: str, content: dict):
-    get_src_folder(project_path).joinpath(name).write_text(yaml.safe_dump(content, sort_keys=False, indent=2))
