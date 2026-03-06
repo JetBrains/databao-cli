1aec87f9d1f4eae12b7da13980c75a7c07f56f24
ruslan.golov <ruslan.golov@jetbrains.com>
2026-03-06 18:28:07 +0100

    e2e-tests: small refactoring

--- e2e-tests/src/databases/snowflake_utils.py
+++ e2e-tests/src/databases/snowflake_utils.py
@@ -1,4 +1,3 @@
-from asyncio import Protocol
 from dataclasses import dataclass
 
 from dotenv import load_dotenv
@@ -6,26 +5,13 @@ from utils.pexpect_utils import child_answer, child_answer_safe
 
 load_dotenv()
 
-SN_ACCOUNT = """{{ env_var("SNOWFLAKE_ACCOUNT") }}"""
-SN_USER = """{{ env_var("SNOWFLAKE_TEST_USER") }}"""
-SN_PASSWORD = """{{ env_var("SNOWFLAKE_TEST_USER_PROGRAM_TOKEN") }}"""
-
-
-class SnowflakeAuth(Protocol):
-    def auth_type_name(self) -> str: ...
-
-    def apply(self, child) -> None: ...
-
 
 @dataclass(frozen=True)
 class SnowflakePasswordAuth:
     password: str | None = None
 
-    def auth_type_name(self) -> str:
-        return "SnowflakePasswordAuth"
-
     def apply(self, child) -> None:
-        child_answer(child, r"connection\.auth\.type\?", self.auth_type_name())
+        child_answer(child, r"connection\.auth\.type\?", "SnowflakePasswordAuth")
         child_answer_safe(child, r"connection\.auth\.password\? \(Optional\):", self.password)
 
 
@@ -35,11 +21,8 @@ class SnowflakeKeyPairAuth:
     private_key_file_pwd: str | None = None
     private_key: str | None = None
 
-    def auth_type_name(self) -> str:
-        return "SnowflakeKeyPairAuth"
-
     def apply(self, child) -> None:
-        child_answer(child, r"connection\.auth\.type\?", self.auth_type_name())
+        child_answer(child, r"connection\.auth\.type\?", "SnowflakeKeyPairAuth")
         child_answer_safe(child, r"connection\.auth\.private_key_file\? \(Optional\) :", self.private_key_file)
         child_answer_safe(child, r"connection\.auth\.private_key_file_pwd\? \(Optional\) :", self.private_key_file_pwd)
         child_answer_safe(child, r"connection\.auth\.private_key\? \(Optional\) :", self.private_key)
@@ -49,11 +32,8 @@ class SnowflakeKeyPairAuth:
 class SnowflakeSSOAuth:
     authenticator: str | None = None
 
-    def auth_type_name(self) -> str:
-        return "SnowflakeSSOAuth"
-
     def apply(self, child) -> None:
-        child_answer(child, r"connection\.auth\.type\?", self.auth_type_name())
+        child_answer(child, r"connection\.auth\.type\?", "SnowflakeKeyPairAuth")
         child_answer_safe(child, r"connection\.auth\.authenticator\? \(Optional\) :", self.authenticator)
 
 
@@ -69,3 +49,15 @@ class SnowflakeDB:
     auth: SnowflakePasswordAuth | SnowflakeKeyPairAuth | SnowflakeSSOAuth = SnowflakePasswordAuth()
     check_connection: bool = False
     check_connection_succeed: bool = True
+
+
+def get_working_snowflake_connection() -> SnowflakeDB:
+    return SnowflakeDB(
+        datasource_name="my_test_snowflake",
+        account="""{{ env_var("SNOWFLAKE_ACCOUNT") }}""",
+        warehouse="DEMO_WH",
+        database="DATALORE",
+        user="""{{ env_var("SNOWFLAKE_TEST_USER") }}""",
+        auth=SnowflakePasswordAuth(password="""{{ env_var("SNOWFLAKE_TEST_USER_PROGRAM_TOKEN") }}"""),
+        check_connection=True,
+    )
