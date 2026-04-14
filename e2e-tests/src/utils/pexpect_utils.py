2cf2d764d5b022df03d0e7ebcb56cbc930379bee
Julien Arzul <julien.arzul@jetbrains.com>
2026-04-14 16:44:37 +0200

    Fix e2e-tests (#97)
    * Provide a bit more information about what timed out when expecting a prompt
    
    * Add new prompt expected when running any interactive flow to add a datasource

--- e2e-tests/src/utils/pexpect_utils.py
+++ e2e-tests/src/utils/pexpect_utils.py
@@ -1,17 +1,23 @@
 import allure
-from pexpect import spawn
+from pexpect import TIMEOUT, spawn
 from pexpect.popen_spawn import PopenSpawn
 
 
 @allure.step("Answering prompt '{pattern}' with '{value}'")
 def child_answer(child: PopenSpawn | spawn, pattern: str, value: str | None) -> None:
-    child.expect(pattern)
+    try:
+        child.expect(pattern)
+    except TIMEOUT as e:
+        raise AssertionError(f"Timed out while waiting for a prompt with the pattern {pattern}") from e
     child.sendline("" if value is None else str(value))
 
 
 @allure.step("Answering sensitive prompt '{pattern}'")
 def child_answer_safe(child: PopenSpawn | spawn, pattern: str, value: str | None) -> None:
-    child.expect(pattern)
+    try:
+        child.expect(pattern)
+    except TIMEOUT as e:
+        raise AssertionError(f"Timed out while waiting for a prompt with the pattern {pattern}") from e
     send_secret(child, "" if value is None else str(value))
 
 
